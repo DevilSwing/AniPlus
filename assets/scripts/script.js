f1430d33139d92d92f38a39de0cb69d73e74efb9
bananaWeb <a89.volkov@gmail.com>
2018-05-28 18:59:03 +0300

    progr

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	const client_secret = 'F6wh6xdtLeyH1KksLmIA';
 	const client_id = 'grenka-ynakp';
 	let access_token = '';
+	const listOfAnime = [];
 
 	const animeListBlock = $('.anime-list');
 
@@ -15,29 +16,34 @@ $(document).ready(function(){
 
 	$.post('https://anilist.co/api/auth/access_token', params, function(result){
 		access_token = result.access_token;
+
+		getAniList();
 	});
 
 	function getAniList (){
 		$.get('https://anilist.co/api/browse/anime?access_token=' + access_token, function(result){
+			console.log(result);
 			showAnimeList(result);
-		});
-
-		
-			
+		});		
 	}
-	
-	$('button').click(function() {
-		getAniList();
+
+
+	$('.b_1').click(function() {
+
 	})
 
 	function showAnimeList(animeList) {
 		for (var i = 0; i < animeList.length; i++) {
-			animeListBlock.append(getAnimePreview(animeList[i]));
+			animeListBlock.append(getAnimePreview(animeList[i]) + getAnimeExtra(animeList[i]));
 		}
 	}
 
 	function getAnimePreview(anime) {
-		return '<div class="anime-preview"><div class="anime-preview__left"><img  src="' + anime.image_url_lge + '"></div><div class="anime-preview__right"><div class="anime-preview__title">' + anime.title_romaji + '</div><div class="anime-preview__genres"><span>Genres</span>' + getGenres(anime.genres) + '</div><div class="anime-preview__airing_status"><span>Airing Status</span>: ' + anime.airing_status + '</div><div class="anime-preview__total_episodes"><span>Episodes</span>: ' + anime.total_episodes + '</div></div></div>';
+		return '<div class="anime-preview"><div class="anime-preview__wraped"><div class="anime-preview__left"><img  src="' + anime.image_url_lge + '"></div><div class="anime-preview__right"><div class="anime-preview__title">' + anime.title_romaji + '</div><div class="anime-preview__genres"><span>Genres</span>: ' + getGenres(anime.genres) + '</div><div class="anime-preview__airing_status"><span>Airing Status</span>: ' + anime.airing_status + '</div><div class="anime-preview__total_episodes"><span>Episodes</span>: ' + anime.total_episodes + '</div></div></div>';
+	}
+
+	function getAnimeExtra(anime) {
+		return '<div class="anime-preview__unwraped"></div></div>';
 	}
 
 	function getGenres(genres) {
@@ -53,5 +59,9 @@ $(document).ready(function(){
 
 		return result;
 	}
+
+	$('.anime-list').on('click', '.anime-preview', function() {
+		$(this).toggleClass('-open');
+	})
 
 });
